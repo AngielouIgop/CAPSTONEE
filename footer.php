0ae2cf550774f3172b7c6e5131f97f47ca0f4ef2
AngielouIgop <sujedeangielou06@gmail.com>
2025-08-14 08:58:42 +0800

    Adding all capstone codes from local repo to this repo

--- footer.php
+++
@@ -1,9 +0,0 @@
-
-<link rel="stylesheet" href="css/header-footer.css">
-<body>
-    <footer class="footer-main">
-        <div class="footer-content">
-            <p>&copy; 2025 BASURA APP. All rights reserved.</p>
-        </div>
-    </footer>
-    </body>
